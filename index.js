1c6c30754dea0e61b1e21421e8e364b111849cfa
Guindoservice <abdala.guindo@odc.local>
2024-04-11 15:22:41 +0000

    ok

---
+++ index.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+  // Chargement des données depuis le localStorage
+    var TASK= JSON.parse(localStorage.getItem("TASK")) || [];
+    // Affichage initial des données
+   Afficher();
+   
+  // Gestion de la soumission du formulaire
+   $(".Formulaire").submit(function(e){
+    e.preventDefault();
+    var date = $("#date").val();
+    var tache = $("#tache").val();
+    TASK.push({date:date, tache:tache});
+    localStorage.setItem("TASK",JSON.stringify(TASK));
+
+    Afficher();
+    $("#date").val();
+    $("#tache").val();
+
+   });
+
+   // fonction pour afficher
+   function Afficher(){
+    $("#tablebody").empty();
+    TASK.forEach(function(addTast, index){
+        $("#tablebody").append(`
+        <tr>
+          <td>${addTast.date}</td>
+          <td>${addTast.tache}</td>
+          <td><input type="checkbox" class="statut"></td>
+          <td><button class="dange" id ="action" data-index="${index}"> <i class="fas fa-trash-alt"></i> </button></td>
+        </tr>
+        `);
+    });
+   }
+   // la partie suppression 
+   $(document).on("click",".dange",function(){
+    let index= $(this).data("index");
+    TASK.splice(index,1);
+    localStorage.setItem("TASK",JSON.stringify(TASK));
+    Afficher();
+   });
+   // la partie masque et afficher
+   $(document).on("change", ".statut", function() {
+    $(this).closest("tr").toggleClass("lignbarre");
+    // localStorage.setItem("TASK",JSON.stringify(TASK));
+    // Afficher();
+});
+
+});
